51afc34d6fb63262811de359d73a6cc9a152e8b0
belaid-hanaa <hanaabelaid098@gmail.com>
2025-03-16 14:25:23 +0000

    about

--- src/components/Home/about.js
+++ src/components/Home/about.js
@@ -1,105 +1,33 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/me.jpg";
 import Tilt from "react-parallax-tilt";
-import { MdOutlineEmail } from "react-icons/md";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+
 
 function about() {
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
+        
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            <h1 style={{ marginLeft:"-200px", fontSize: "3.6em" }}>
+            About Me
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
-            </p>
+            I’m Hanaa Belaid, a first-year Master's student in Artificial Intelligence and Data Science at FSTT, Tangier. Passionate about technology, I’m focusing on AI, machine learning, and data science to solve real-world problems.
+            I have a strong foundation in programming and data analysis, and I'm eager to apply my skills in deep learning and NLP. Outside of my studies, I actively engage in tech-related events and workshops.
+            Feel free to explore my work and connect with me for collaborations!</p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
+          
+          
         </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/belaid-hanaa"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/hanaa-belaid-483166289/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="mailto:hanaabelaid098@gmail.com"
-                  style={{ color: "white" }}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <MdOutlineEmail />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+        
       </Container>
     </Container>
   );
