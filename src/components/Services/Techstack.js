0c3e0d4d07b727202e7c3ec8f231f145d09a7ed1
belaid-hanaa <hanaabelaid098@gmail.com>
2025-03-17 22:57:38 +0000

    fin service

--- src/components/Services/Techstack.js
+++ src/components/Services/Techstack.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 
 // Importer les images correspondantes à chaque technologie
-import Image from "../../Assets/avatar.svg";
+import fstt from "../../Assets/fstt.png";
+import te from "../../Assets/TE.jpeg";
+import Cosumar from "../../Assets/cosumar.png";
+import lgd from "../../Assets/lgd.jpg";
+import charity from "../../Assets/lgd2.jpg";
 function Techstack() {
   const techs = [
-    { img: Image, title: "C++", description: "A powerful language for system and application programming." },
-    { img: Image, title: "JavaScript", description: "The language of the web for dynamic client-side programming." },
-    { img: Image, title: "Go", description: "A statically typed, compiled language for scalable applications." },
-    { img: Image, title: "Node.js", description: "JavaScript runtime for building fast and scalable server-side applications." },
-    { img: Image, title: "React", description: "A library for building user interfaces with reusable components." },
+    { img: fstt, title: "Faculté des Sciences et Techniques", description: "At FSTT, I developed expertise in web development (Laravel,django,Fastapi, React) and artificial intelligence (machine learning, deep learning...)" },
+    { img: te, title: "TE Connectivity", description: "During my internship at TE Connectivity, I gained hands-on experience in data analytics and engineering, working with Python, SQL, and machine learning to extract insights and optimize operations." },
+    { img: Cosumar, title: "Cosumar", description: "At Cosumar, my observational internship provided knowledge in network infrastructure, data architecture, and IT systems, enhancing my understanding of enterprise networks." },
+    { img: lgd, title: "The Legends Club", description: "As Head of the IT Department, I lead technical projects and mentor members. I also coordinate training sessions on Spring Boot, Angular, and AI, ensuring effective skill development." },
+    { img: charity, title: "Charity Work", description: "I am committed to giving back to the community through volunteer work and charitable activities, offering my skills and time to support social initiatives and make a positive impact." },
    
   ];
 
